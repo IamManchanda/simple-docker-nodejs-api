fc9953e60073240af3298ee4ba9cc03f6957917c
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-12-06 22:28:28 +0530

    Connect MongoDB

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ var apiController = require("./controllers/apiController");
 
 var port = 3000;
 
+var { DATABASE_USERNAME, DATABASE_PASSWORD } = process.env;
+
 app.use("/assets", express.static(__dirname + "/public"));
-//mongo instead of localhost for docker-compose
+
+// mongo instead of mongo atlas for docker-compose
 mongoose.connect(
-  "mongodb://localhost:27017/users",
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@cluster-000.3uokz.mongodb.net/users?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
   (err) => {
     console.log(err);
   },
