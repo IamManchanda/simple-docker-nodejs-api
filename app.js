e8d16ed2dae03e8c0a6352aeedb340a1aabe3086
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-12-07 15:13:50 +0530

    code changes

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var apiController = require("./controllers/apiController");
 
 var port = 3000;
 
-var { DATABASE_USERNAME, DATABASE_PASSWORD } = process.env;
+/* var { DATABASE_USERNAME, DATABASE_PASSWORD } = process.env; */
 
 app.use("/assets", express.static(__dirname + "/public"));
 
-// mongo instead of mongo atlas for docker-compose
+// mongo instead of localhost for docker-compose
+// `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@cluster-000.3uokz.mongodb.net/users?retryWrites=true&w=majority`,
 mongoose.connect(
-  `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@cluster-000.3uokz.mongodb.net/users?retryWrites=true&w=majority`,
+  "mongodb://localhost:27017/users",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
